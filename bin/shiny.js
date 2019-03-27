959c5ab0bc23c57746e90ed99353b5023630047f
Yeung Yiu Hung <hkclex@gmail.com>
2019-03-27 16:44:08 +0800

    Add custom count

--- bin/shiny.js
+++ bin/shiny.js
@@ -6,9 +6,27 @@ const path = require("path");
 const args = process.argv.slice(2);
 
 if (args.length !== 0) {
-  if (args.length !== 1) {
-    console.log("Usage: shiny [pokemon name]");
+  if (args.length < 1) {
+    console.log("Usage: shiny [pokemon name] [number (optional)]");
   } else {
+    let inc = 1;
+
+    if (args.length === 2) {
+      try {
+        inc = parseInt(args[1]);
+
+        if (inc < 1) {
+          console.error("Must be greater than 1");
+          return;
+        }
+      } catch (e) {
+        console.error("Not a number");
+
+        console.error(e);
+        return;
+      }
+    }
+
     let number = 0;
 
     const huntProgress = path.join(process.cwd(), args[0]);
@@ -16,7 +34,7 @@ if (args.length !== 0) {
       number = parseInt(fs.readFileSync(huntProgress).toString());
     } catch {}
 
-    number++;
+    number = number + inc;
 
     console.log(`Hunting ${args[0]} for ${number} times`);
 
